e21303d44ba4b6884df96a8a7e66a0632b314995
Hardik Khatri <hardikkhatri1312@gmail.com>
2023-06-08 15:53:29 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,12 @@
+require("@nomiclabs/hardhat-ethers");
+require("@openzeppelin/hardhat-upgrades");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.6",
+  networks:{
+    hardhat:{
+      chainId:1337
+    }
+  }
+};
