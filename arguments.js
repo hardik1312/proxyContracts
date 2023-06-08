a64b9002f721df32301e41c0cc352468a7bfdf62
Hardik Khatri <hardikkhatri1312@gmail.com>
2023-06-08 22:10:43 +0530

    added timelock sc, deploy-timelock script, etc

---
+++ arguments.js
@@ -0,0 +1,5 @@
+module.exports = [
+    150,
+    ["0x0fbe60AF0F825Ea5A5062916FDaCF18250B00383"], // proposers array
+    ["0x0fbe60AF0F825Ea5A5062916FDaCF18250B00383"] // executors array
+]
--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,10 @@ module.exports = {
   networks:{
     hardhat:{
       chainId:1337
+    },
+    goerli:{
+      url:'https://goerli.infura.io/v3/bc0e451f336944e2b2ab25a161076ef4', 
+      accounts:['ea2497681e7e98a8c0bca14f836bcf97fdc3ad9f75956acf8feefcc1c5a4d789']
     }
   }
 };
